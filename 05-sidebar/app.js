7cd108eb8a282aaad3158bd478a36b107db68b35
christima <christimamary@gmail.com>
2022-01-25 14:10:38 +0530

    project-5

---
+++ 05-sidebar/app.js
@@ -0,0 +1,11 @@
+const sidebar = document.querySelector(".sidebar");
+const toggleButton = document.querySelector(".sidebar-toggle");
+const closeButton = document.querySelector(".close-btn");
+
+toggleButton.addEventListener("click", () =>
+  sidebar.classList.toggle("show-sidebar")
+);
+
+closeButton.addEventListener("click", () =>
+  sidebar.classList.remove("show-sidebar")
+);
